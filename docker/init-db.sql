b153c65db54909d0ca4e9875b776fdfeab16e81a
GiaBaor <baozach123@gmail.com>
2024-11-02 22:23:27 +0700

    feat: Create basic docker compose file, env file

---
+++ docker/init-db.sql
@@ -0,0 +1,7 @@
+-- CREATE DATABASE database1;
+-- CREATE DATABASE database2;
+--
+-- GRANT ALL PRIVILEGES ON DATABASE database1 TO admin;
+-- GRANT ALL PRIVILEGES ON DATABASE database2 TO admin;
+
+-- TODO: create multi schemas in same database or separate schemas to totally different database?
