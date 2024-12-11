0e7a9aff13fb13e28b4bda0625a5e09251090799
Tran Gia Bao <bao.trangia@elca.vn>
2024-12-11 18:14:06 +0700

    feat: Update BaseEnum, EnumUtils

--- sep490-common/src/main/java/sep490/common/api/security/UserRole.java
+++ sep490-common/src/main/java/sep490/common/api/security/UserRole.java
@@ -3,25 +3,18 @@ package sep490.common.api.security;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.lang3.StringUtils;
+import sep490.common.api.BaseEnum;
 
 import java.util.Arrays;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
-public enum UserRole {
+public enum UserRole implements BaseEnum {
     ENTERPRISE_OWNER(RoleNameConstant.ENTERPRISE_OWNER),
     SYSTEM_ADMIN(RoleNameConstant.SYSTEM_ADMIN);
     
-    private final String roleValue;
-    
-    
-    public static UserRole fromValue(String value) {
-        return Arrays.stream(UserRole.values())
-                     .filter(role -> StringUtils.equals(role.getRoleValue(), value))
-                     .findAny()
-                     .orElse(null);
-    }
+    private final String code;
+
     
     public static final class RoleNameConstant {
         public static final String ENTERPRISE_OWNER = "sep390.EnterpriseOwner";
