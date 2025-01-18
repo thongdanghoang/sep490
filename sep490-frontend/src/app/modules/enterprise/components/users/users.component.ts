7eb0eb9a33beedb50af73f3c7cd7b79e7781e1b3
Tran Gia Bao <bao.trangia@elca.vn>
2025-01-18 12:54:49 +0700

    feat: adapt eslint

--- sep490-frontend/src/app/modules/enterprise/components/users/users.component.ts
+++ sep490-frontend/src/app/modules/enterprise/components/users/users.component.ts
@@ -1,66 +1,101 @@
-import { Component, EventEmitter, OnInit, TemplateRef, ViewChild } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { SearchCriteriaDto, SearchResultDto } from '../../../shared/models/models';
-import { ApplicationService } from '../../../core/services/application.service';
-import { Observable } from 'rxjs';
-import { EnterpriseUserDTO } from '../../../shared/models/business-model';
-import { TableTemplateColumn } from '../../../shared/components/table-template/table-template.component';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  TemplateRef,
+  ViewChild
+} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {
+  SearchCriteriaDto,
+  SearchResultDto
+} from '../../../shared/models/models';
+import {ApplicationService} from '../../../core/services/application.service';
+import {Observable} from 'rxjs';
+import {EnterpriseUserDTO} from '../../../shared/models/business-model';
+import {TableTemplateColumn} from '../../../shared/components/table-template/table-template.component';
+import {environment} from '../../../../../environments/environment';
 
 @Component({
-    selector: 'app-users',
-    templateUrl: './users.component.html',
-    styleUrl: './users.component.css'
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.css'
 })
 export class UsersComponent implements OnInit {
+  @ViewChild('roleTemplate', {static: true}) roleTemplate!: TemplateRef<any>;
+  @ViewChild('scopeTemplate', {static: true}) scopeTemplate!: TemplateRef<any>;
+  @ViewChild('actionsTemplate', {static: true})
+  actionsTemplate!: TemplateRef<any>;
 
-    protected fetchUsers!: (criteria: SearchCriteriaDto<string>) => Observable<SearchResultDto<EnterpriseUserDTO>>;
-    protected cols: TableTemplateColumn[] = [];
-    protected readonly searchEvent: EventEmitter<void> = new EventEmitter();
-    protected selected: EnterpriseUserDTO[] = [];
-    protected searchCriteria: string = '';
+  protected fetchUsers!: (
+    criteria: SearchCriteriaDto<string>
+  ) => Observable<SearchResultDto<EnterpriseUserDTO>>;
+  protected cols: TableTemplateColumn[] = [];
+  protected readonly searchEvent: EventEmitter<void> = new EventEmitter();
+  protected selected: EnterpriseUserDTO[] = [];
+  protected searchCriteria: string = '';
 
-    @ViewChild("roleTemplate", {static: true}) roleTemplate!: TemplateRef<any>;
-    @ViewChild("scopeTemplate", {static: true}) scopeTemplate!: TemplateRef<any>;
-    @ViewChild('actionsTemplate', { static: true }) actionsTemplate!: TemplateRef<any>;
+  constructor(
+    private readonly httpClient: HttpClient,
+    protected readonly applicationService: ApplicationService
+  ) {}
 
-    constructor(private readonly httpClient: HttpClient,
-                protected readonly applicationService: ApplicationService) {
-    }
+  ngOnInit(): void {
+    this.buildCols();
+    this.fetchUsers = (
+      criteria
+    ): Observable<SearchResultDto<EnterpriseUserDTO>> => {
+      return this.httpClient.post<SearchResultDto<EnterpriseUserDTO>>(
+        `${environment.idpApiUrl}/api/enterprise-user/search`,
+        criteria
+      );
+    };
+  }
 
-    ngOnInit(): void {
-        this.buildCols();
-        this.fetchUsers = (criteria) => {
-            return this.httpClient.post<SearchResultDto<EnterpriseUserDTO>>('http://localhost:8080/api/enterprise-user/search', criteria);
-        };
-    }
+  buildCols(): void {
+    this.cols.push({
+      field: 'name',
+      sortField: 'firstName',
+      header: 'enterprise.Users.table.name',
+      sortable: true
+    });
+    this.cols.push({
+      field: 'email',
+      header: 'enterprise.Users.table.email',
+      sortable: true
+    });
+    this.cols.push({
+      field: 'role',
+      header: 'enterprise.Users.table.role',
+      sortable: true,
+      templateRef: this.roleTemplate
+    });
+    this.cols.push({
+      field: 'scope',
+      header: 'enterprise.Users.table.scope',
+      sortable: true,
+      templateRef: this.scopeTemplate
+    });
+    this.cols.push({
+      field: 'actions',
+      header: '',
+      templateRef: this.actionsTemplate
+    });
+  }
 
-    buildCols(): void {
-        this.cols.push({field: 'name', sortField: "firstName", header: 'enterprise.Users.table.name', sortable: true});
-        this.cols.push({field: 'email', header: 'enterprise.Users.table.email', sortable: true});
-        this.cols.push({field: 'role', header: 'enterprise.Users.table.role', sortable: true, templateRef: this.roleTemplate});
-        this.cols.push({field: 'scope', header: 'enterprise.Users.table.scope', sortable: true, templateRef: this.scopeTemplate});
-        this.cols.push({field: 'actions', header: '', templateRef: this.actionsTemplate})
-    }
+  onSelectionChange(selectedUsers: EnterpriseUserDTO[]): void {
+    this.selected = selectedUsers;
+  }
 
-    onSelectionChange(selectedUsers: EnterpriseUserDTO[]): void {
-        this.selected = selectedUsers;
-    }
+  search(): void {
+    this.searchEvent.emit();
+  }
 
-    search(): void {
-        this.searchEvent.emit();
-    }
+  confirmDelete(): void {}
 
-    confirmDelete(): void {
-        console.log(this.selected);
-    }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars,@typescript-eslint/explicit-function-return-type
+  onDelete(rowData: TableTemplateColumn) {}
 
-    onDelete(rowData: TableTemplateColumn) {
-        console.log(rowData);
-    }
-
-    onEdit(rowData: TableTemplateColumn) {
-
-    }
-
-
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type,@typescript-eslint/no-unused-vars
+  onEdit(rowData: TableTemplateColumn) {}
 }
