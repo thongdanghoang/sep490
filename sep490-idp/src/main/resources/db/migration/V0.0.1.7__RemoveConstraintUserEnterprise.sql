1f3ba7d707f7df007c7cacd6646e6deb13dcf76b
Tran Gia Bao <bao.trangia@elca.vn>
2025-02-08 17:37:13 +0700

    feature: update foreign key of user enterprise

--- sep490-idp/src/main/resources/db/migration/V0.0.1.7__RemoveConstraintUserEnterprise.sql
+++ sep490-idp/src/main/resources/db/migration/V0.0.1.7__RemoveConstraintUserEnterprise.sql
@@ -1,1 +1,2 @@
 ALTER TABLE enterprise_users DROP CONSTRAINT enterprise_users_fk_user;
+ALTER TABLE enterprise_users ADD CONSTRAINT enterprise_users_fk_user FOREIGN KEY (user_id) REFERENCES users (id);
