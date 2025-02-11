85bce07c5c2f8edb58cf62cd6c20873ac72dd1b4
Thống <work@thongdanghoang.id.vn>
2025-02-12 04:08:12 +0700

    workaround: @SoftDelete didn't work with Lazy Load

--- sep490-idp/src/main/java/green_buildings/idp/consumers/IdPEventConsumer.java
+++ sep490-idp/src/main/java/green_buildings/idp/consumers/IdPEventConsumer.java
@@ -4,14 +4,14 @@ import green_buildings.commons.api.events.CommitEnterpriseRegisterEvent;
 import green_buildings.commons.api.events.RollbackEnterpriseRegisterEvent;
 import green_buildings.commons.api.exceptions.BusinessErrorResponse;
 import green_buildings.commons.api.utils.MDCContext;
+import green_buildings.idp.dto.SignupDTO;
+import green_buildings.idp.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
-import green_buildings.idp.dto.SignupDTO;
-import green_buildings.idp.service.UserService;
 
 import java.nio.charset.StandardCharsets;
 import java.util.UUID;
@@ -36,7 +36,6 @@ public class IdPEventConsumer {
     public void rollbackEnterpriseOwnerCreationEvent(ConsumerRecord<String, Object> consumerRecord) {
         if (consumerRecord.value() instanceof RollbackEnterpriseRegisterEvent event) {
             userService.rollbackEnterpriseOwnerCreation(new BusinessErrorResponse(getCorrelationId(consumerRecord), event.field(), event.i18nKey(), event.args()));
-            return;
         }
     }
     
