5d50b021da4d73028bd705dcc5baceb2a47f2e58
Tran Gia Bao <bao.trangia@elca.vn>
2025-02-15 13:18:31 +0700

    feature: Fix password regex

--- sep490-commons/api/src/main/java/green_buildings/commons/api/utils/CommonConstant.java
+++ sep490-commons/api/src/main/java/green_buildings/commons/api/utils/CommonConstant.java
@@ -10,7 +10,7 @@ public final class CommonConstant {
             + "(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:"
             + "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])$";
 
-    public static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+    public static final String PASSWORD_PATTERN = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d\\W]{8,}$";
     // 8 characters at least one letter and one number
 
     public static final String VIETNAM_PHONE_PATTERN = "^[0]{1}[0-9]{9}$";
