3e91ad3dfeba36eda23f7b1ce7207a599d175a55
Tran Gia Bao <bao.trangia@elca.vn>
2025-03-01 00:30:51 +0700

    feature: improve query, bugfix, i18n add

--- sep490-enterprise/src/main/java/greenbuildings/enterprise/repositories/BuildingRepository.java
+++ sep490-enterprise/src/main/java/greenbuildings/enterprise/repositories/BuildingRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface BuildingRepository extends AbstractBaseRepository<BuildingEntity> {
@@ -13,5 +14,7 @@ public interface BuildingRepository extends AbstractBaseRepository<BuildingEntit
     Page<BuildingEntity> findByEnterpriseId(UUID enterpriseId, Pageable pageable);
     
     List<BuildingEntity> findAllByEnterpriseId(UUID enterpriseId);
+    
+    Optional<BuildingEntity> findByIdAndEnterpriseId(UUID id, UUID enterpriseId);
     
 }
